a10592160430e23c7fdecff07e16a3782d2cb990
無限 <49502834+owo2166hz@users.noreply.github.com>
2023-08-07 12:32:15 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,69 +1,84 @@
+'''
+Author: owo2166hz owo2166hz@gmail.com
+Date: 2023-08-07 12:26:43
+LastEditors: owo2166hz owo2166hz@gmail.com
+LastEditTime: 2023-08-07 12:28:19
+FilePath: \OWOc:\Users\owo21\Downloads\app.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 from flask import Flask, request, abort
-from linebot.exceptions import (InvalidSignatureError)
 from linebot import (LineBotApi, WebhookHandler, exceptions)
+from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-line_bot_api = LineBotApi('/5O5W/lf2xUg2O4/x/whu8JrKyoN4LzoExue5u+JTJmrOOZlkYq+KDSiW/lDhAInEIAr1tiNT8r4IC71DYNf7cEB95E7kB63JAh/Q+jyMFE3IaMy4hc9FgKjPUx6GrWrbSmgPbXHYbO2VLkt3vJQ5wdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('9110abd2b90e95aa80c4cb30023507d7')
+#自己的Channel Access Token(在Messaging API底下)
+line_bot_api = LineBotApi("wR6RWNy+d1LEYGfPCD0AbGehrEI+cPTQxChN5KftrpfD7JZbuwKIoj1Ys41AL8+S2tehpIAOJVeZihxBVyZnMi8YPeHpQT9PeMRzc+UfkGwoxcSYIc9H+5yLPh3HSvsR4cagMIIFHybDESjA0+CiewdB04t89/1O/w1cDnyilFU=")
+#自己的Channel secret(在Basic Settings底下)
+handler = WebhookHandler("5c7720c80fc66096c32f76253778ded8")
 
-
-
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
+#監聽所有來自/callback的Post Request
+@app.route("/callback", methods=["POST"])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    #get X-Line-Signature header value
+    signature = request.headers["X-Line_Signature"]
 
-    # get request body as text
+    #get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    #header webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return "OK"
 
-# 處理訊息
+#處理訊息(這邊是回傳同樣的訊息)
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     message = TextSendMessage(text=event.message.text)
+#     line_bot_api.reply_message(event.reply_token, message)
+
+#處理訊息(這邊是歡迎訊息)
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
     emoji = [
-            {
-                "imdex":0,
-                "productId":"5ac21184040ab15980c9b43a",
-                "emojiId":"001"
-            },
-            {
-                "imdex":18,
-                "productId":"5ac21184040ab15980c9b43a",
-                "emojiId":"001"
-            }
-    ]
+            {"index":0,
+             "productId":"5ac21b4f031a6752fb806d59",
+             "emojiId":"011"
+                },
 
-
-    text_message = TextSendMessage(text='''$ Master Finance $
-Hello! 您好 歡迎您成為OWO的好友
-                                   
-我是OWO財經小幫手
-                                   
--這裡有股票 匯率資訊
--直接點選下方的選單功能
--期待您的光臨''',emojis=emoji)
+            {"index":1,
+             "productId":"5ac21b4f031a6752fb806d59",
+             "emojiId":"035"
+                },
+            {"index":2,
+             "productId":"5ac21b4f031a6752fb806d59",
+             "emojiId":"049"
+                },
+            {"index":3,
+             "productId":"5ac21b4f031a6752fb806d59",
+             "emojiId":"035"
+                },
+            ]
+    #歡迎訊息
+    text_message = TextSendMessage(text="""$$$$
+Hello! 恭喜您成為kiwi機器人的第100個好友
+這邊提供了一些功能，這段只是歡迎訊息而已
+才不希望你點進來呢！""",emojis=emoji)
     
+    #貼圖訊息
     sticker_message = StickerMessage(
         package_id = "11537",
         sticker_id = "52002769")
+    
+    #將歡迎訊息、貼圖訊息放到機器人裡面回復
     line_bot_api.reply_message(
         event.reply_token,
         [text_message, sticker_message])
-    
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run()
