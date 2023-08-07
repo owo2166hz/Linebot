1869001b746e38bbf1cdb54eab96b4e3a0b5cf4a
無限 <49502834+owo2166hz@users.noreply.github.com>
2023-08-07 10:58:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def callback():
 
     return 'OK'
 
-@handler.add(MessageEvent,nessage = TextMessage)
+@handler.add(MessageEvent,message = TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token,message)
