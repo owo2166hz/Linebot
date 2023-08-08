b77cb02c1d51a6f984664f749b70cd4695ab2f63
owo2166hz <owo2166hz@gmail.com>
2023-08-08 17:42:18 +0800

    123

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 Author: owo2166hz owo2166hz@gmail.com
 Date: 2023-08-07 09:38:44
 LastEditors: owo2166hz owo2166hz@gmail.com
-LastEditTime: 2023-08-08 11:31:06
+LastEditTime: 2023-08-08 16:26:25
 FilePath: \OWO\LINEBOT\app.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
