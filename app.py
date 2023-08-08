ff3d3328657f2d313dd92c106c662990194a8d88
owo2166hz <owo2166hz@gmail.com>
2023-08-08 11:25:52 +0800

    if event.message.text in ['小幫手', '幫手']:

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 Author: owo2166hz owo2166hz@gmail.com
 Date: 2023-08-07 09:38:44
 LastEditors: owo2166hz owo2166hz@gmail.com
-LastEditTime: 2023-08-07 16:49:45
+LastEditTime: 2023-08-08 11:25:18
 FilePath: \OWO\LINEBOT\app.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -49,7 +49,7 @@ def handle_message(event):
         Usage(event)
     
         
-    if event.message.text == '小幫手':
+    if event.message.text in ['小幫手', '幫手']:
         line_bot_api.reply_message(event.reply_token , buttons_template)
 
     ############################    OWO0    ############################
