daf0d8beac419d7690b9a1752fb556aeb592dbbb
owo2166hz <owo2166hz@gmail.com>
2023-08-09 14:27:06 +0800

    123

--- model/mongodb.py
+++ model/mongodb.py
@@ -2,7 +2,7 @@
 Author: owo2166hz owo2166hz@gmail.com
 Date: 2023-08-09 11:32:52
 LastEditors: owo2166hz owo2166hz@gmail.com
-LastEditTime: 2023-08-09 14:18:24
+LastEditTime: 2023-08-09 14:27:04
 FilePath: \Linebot-1\model\mongodb.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -43,4 +43,4 @@ def write_my_stock(userID, user_name, stockNumber, condition , target_price):
                 "tag": "stock",
                 "date_info": datetime.datetime.now()
             })
-        return f"{stockNumber}已新增至您的股票清單"
+    return f"{stockNumber}已新增至您的股票清單"
