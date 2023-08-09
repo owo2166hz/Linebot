8bdeb26b471af3581ec3b8722a99e21c15f20c03
owo2166hz <owo2166hz@gmail.com>
2023-08-09 15:21:28 +0800

    123

--- events/Msg_Template.py
+++ events/Msg_Template.py
@@ -2,7 +2,7 @@
 Author: owo2166hz owo2166hz@gmail.com
 Date: 2023-08-09 09:28:38
 LastEditors: owo2166hz owo2166hz@gmail.com
-LastEditTime: 2023-08-09 10:19:27
+LastEditTime: 2023-08-09 15:21:23
 FilePath: \Linebot-1\events\Msg_Template.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -36,299 +36,299 @@ def show_Button():
     flex_message = FlexSendMessage(
             alt_text="幣別種類",
             contents={
-                "type": "bubble",
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                    {
-                        "type": "text",
-                        "text": "幣別種類",
-                        "weight": "bold",
-                        "size": "xl",
-                        "color": "#AA2B1D"
-                    }
-                    ]
-                },
-                "footer": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "美金",
-                            "text": "USD"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "日圓",
-                            "text": "JPY"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "港幣",
-                            "text": "HKD"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        }
-                        ]
-                    },
-                    {
-                        "type": "separator",
-                        "margin": "md"
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "英鎊",
-                            "text": "GBP"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "澳幣",
-                            "text": "AUD"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "加拿大幣",
-                            "text": "CAD"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        }
-                        ]
-                    },
-                    {
-                        "type": "separator",
-                        "margin": "md"
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "瑞士法郎",
-                            "text": "CHF"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "新加坡",
-                            "text": "SGD"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "南非幣",
-                            "text": "ZAR"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        }
-                        ]
-                    },
-                    {
-                        "type": "separator",
-                        "margin": "md"
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "瑞典幣",
-                            "text": "SEK"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "泰幣",
-                            "text": "THB"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "菲比索",
-                            "text": "PHP"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        }
-                        ]
-                    },
-                    {
-                        "type": "separator",
-                        "margin": "md"
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "印尼幣",
-                            "text": "IDR"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "韓元",
-                            "text": "KRW"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "馬來幣",
-                            "text": "MYR"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        }
-                        ]
-                    },
-                    {
-                        "type": "separator",
-                        "margin": "md"
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "越南盾",
-                            "text": "VND"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "人民幣",
-                            "text": "CNY"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        },
-                        {
-                            "type": "button",
-                            "action": {
-                            "type": "message",
-                            "label": "紐元",
-                            "text": "NZD"
-                            },
-                            "gravity": "center",
-                            "style": "primary",
-                            "color": "#CC561E",
-                            "margin": "sm"
-                        }
-                        ]
-                    }
-                    ]
-                }
-        }
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "幣別種類",
+        "weight": "bold",
+        "size": "xl",
+        "color": "#74098d"
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "美金",
+              "text": "USD"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "日圓",
+              "text": "JPY"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "港幣",
+              "text": "HKD"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          }
+        ]
+      },
+      {
+        "type": "separator",
+        "margin": "md"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "英鎊",
+              "text": "GBP"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "澳幣",
+              "text": "AUD"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "加拿大幣",
+              "text": "CAD"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          }
+        ]
+      },
+      {
+        "type": "separator",
+        "margin": "md"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "瑞士法郎",
+              "text": "CHF"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "新加坡",
+              "text": "SGD"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "南非幣",
+              "text": "ZAR"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          }
+        ]
+      },
+      {
+        "type": "separator",
+        "margin": "md"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "瑞典幣",
+              "text": "SEK"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "泰幣",
+              "text": "THB"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "菲比索",
+              "text": "PHP"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          }
+        ]
+      },
+      {
+        "type": "separator",
+        "margin": "md"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "印尼幣",
+              "text": "IDR"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "韓元",
+              "text": "KRW"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "馬來幣",
+              "text": "MYR"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          }
+        ]
+      },
+      {
+        "type": "separator",
+        "margin": "md"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "越南盾",
+              "text": "VND"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "人民幣",
+              "text": "CNY"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "紐元",
+              "text": "NZD"
+            },
+            "gravity": "center",
+            "style": "primary",
+            "color": "#8d1ab3",
+            "margin": "sm"
+          }
+        ]
+      }
+    ]
+  }
+}
                     
     )
     return flex_message
