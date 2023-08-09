efdbbf4643c52878349b09920833c2c3baa9480b
owo2166hz <owo2166hz@gmail.com>
2023-08-09 13:37:21 +0800

    123

--- model/mongodb.py
+++ model/mongodb.py
@@ -2,7 +2,7 @@
 Author: owo2166hz owo2166hz@gmail.com
 Date: 2023-08-09 11:32:52
 LastEditors: owo2166hz owo2166hz@gmail.com
-LastEditTime: 2023-08-09 13:23:17
+LastEditTime: 2023-08-09 13:36:22
 FilePath: \Linebot-1\model\mongodb.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -16,6 +16,14 @@ def constructor_stock():
     client = MongoClient("mongodb://owo2166hz:<password>@ac-ry9oitq-shard-00-00.tevu9ns.mongodb.net:27017,ac-ry9oitq-shard-00-01.tevu9ns.mongodb.net:27017,ac-ry9oitq-shard-00-02.tevu9ns.mongodb.net:27017/?ssl=true&replicaSet=atlas-m5uf0e-shard-0&authSource=admin&retryWrites=true&w=majority")
     db = client[stockDB]
     return db
+
+def update_my_stock(user_name, stockNumber, condition, target_price):
+    db = constructor_stock()
+    collent = db[user_name]
+    collent.update_many({'favorite_stock': stockNumber},{'$set':{'condition':condition,'price':target_price}})
+    content = f'股票{stockNumber}更新成功'
+    return content
+
 #新增使用者的股票
 def write_my_stock(userID,user_name, stockNumber, condition , target_price):
     db=constructor_stock()
