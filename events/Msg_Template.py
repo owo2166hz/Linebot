56cc61e6fdf63e1c10b6646ac5dd7c129a284189
owo2166hz <owo2166hz@gmail.com>
2023-08-09 10:19:41 +0800

    QuickReplyButton

--- events/Msg_Template.py
+++ events/Msg_Template.py
@@ -1,3 +1,11 @@
+'''
+Author: owo2166hz owo2166hz@gmail.com
+Date: 2023-08-09 09:28:38
+LastEditors: owo2166hz owo2166hz@gmail.com
+LastEditTime: 2023-08-09 10:19:27
+FilePath: \Linebot-1\events\Msg_Template.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 from line_bot_api import *
 
 
@@ -13,7 +21,7 @@ def stock_reply_other(stockNumber):
                                             text="#"+stockNumber,
                                 )
                         ),
-                        quickReplyButton(
+                        QuickReplyButton(
                                 action=MessageAction(
                                     label="K線圖",
                                     text="@K"+stockNumber
