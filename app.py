328561340c8fbfb1a7f54d66900a7f209cee69d7
owo2166hz <owo2166hz@gmail.com>
2023-08-11 16:00:46 +0800

    123

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 Author: owo2166hz owo2166hz@gmail.com
 Date: 2023-08-07 09:38:44
 LastEditors: owo2166hz owo2166hz@gmail.com
-LastEditTime: 2023-08-11 15:54:53
+LastEditTime: 2023-08-11 16:00:34
 FilePath: \OWO\LINEBOT\app.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -163,6 +163,7 @@ def handle_message(event):
     if re.match("關閉提醒", msg):
         import schedule
         schedule.clear()
+        line_bot_api.push_message(uid,TextSendMessage("將為您關閉提醒......"))
     if re.match("股價提醒", msg):
         import schedule
         import time
